4d0dd26fdeeab7aec5d661b1c4151c80b82e8948
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-07-17 01:48:42 +0430

    left neccesary comment.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import "./styles.css";
+import React, { useState } from 'react';
+//fetching data from data.js
+import data from './data'
+//List is a child component so import it here.
+import List from './List'
 
-export default function App() {
+function App() {
+//using from useState Hook and set data to initial state
+  const [people, setPeople] = useState(data)
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        {/*set our data as prop value */}
+        <List people={people} />
+        {/*a button that set people an empty array */}
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  )
 }
+
+export default App;
